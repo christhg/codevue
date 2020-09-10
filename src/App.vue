87f8dfbf621d865af84e046827860add630b5bde
unknown <christ.hg@gmail.com>
2020-09-10 23:39:33 +0800

    toolbar footer content

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar color="#00ADFF" dark>
+      <v-app-bar-nav-icon to="/"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>codevue.io</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down"> 
+      <v-btn flat to="/signup">Sign Up</v-btn>
+
+      <v-btn flat to="/login">Login</v-btn>
+    </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer color="#00ADFF" dark>
+    <v-spacer></v-spacer>
+    <div>codevue.io &nbsp; &copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>   
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+//import HelloWorld from './components/HelloWorld';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    //HelloWorld,
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
